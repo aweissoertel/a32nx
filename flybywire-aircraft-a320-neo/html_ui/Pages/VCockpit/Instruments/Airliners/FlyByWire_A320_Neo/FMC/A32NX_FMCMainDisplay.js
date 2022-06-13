68f6a641738fc85e09e39afa48f220fd90e9e68a
2hwk <15316958+2hwk@users.noreply.github.com>
2022-06-13 22:52:59 +0800

    feat(fms/efb): Pause at distance before top of descent  (#7165)
    * feat(efb): added settings for tod pause
    
    * feat(fms): t/d pause with ap guard and custom t/d pause distance
    
    * meta: changelog
    
    * fix: incorporated suggested improvements

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -4875,6 +4875,10 @@ class FMCMainDisplay extends BaseAirliners {
         return this.thrustReductionAltitude;
     }
 
+    getOriginTransitionAltitude() {
+        return this.flightPlanManager.getOriginTransitionAltitude();
+    }
+
     getCruiseAltitude() {
         return this.cruiseFlightLevel * 100;
     }
